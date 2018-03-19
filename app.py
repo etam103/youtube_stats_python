3ed3d5e4cb398455b1381c4a899b48df9223128e
Evan Tam <evantam@Evans-MacBook-Pro-3.local>
2018-03-19 00:08:05 -0700

    db

--- app.py
+++ app.py
@@ -49,11 +49,11 @@ def make_session_permanent():
   flask.session.permanent = True
   app.permanent_session_lifetime = timedelta(hours=12)
 
-  request = flask.request
-  if request.headers.get('X-Forwarded-Proto') == 'http':
-    url = request.url.replace('http://', 'https://', 1)
-    code = 301
-    return flask.redirect(url, code=code)
+  # request = flask.request
+  # if request.headers.get('X-Forwarded-Proto') == 'http':
+  #   url = request.url.replace('http://', 'https://', 1)
+  #   code = 301
+  #   return flask.redirect(url, code=code)
 
 # Favicon
 # TODO: Test when you are done with project
