6f999c869c4cf685fbb2eda27cb175c15ba1942d
Evan Tam <evantam@Evans-MacBook-Pro-3.local>
2018-03-19 00:50:59 -0700

    db

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ app = flask.Flask(__name__, static_url_path='')
 app.secret_key = '\x03\xc3\x93\x85\xf1F\xdbA\xff\x0b\xc0>:\xff\xa4\x19\x0c\xd6 \xa2G\x1eza'
 
 # Setup Socket IP
-# socketio = SocketIO(app, ping_timeout=100000)
-socketio = SocketIO(app)
+socketio = SocketIO(app, ping_timeout=100000)
+# socketio = SocketIO(app)
 
 # Setup mongodb
 # Seems like storage isn't needed anymore
